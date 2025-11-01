3ef20954709a9fb73ecc050491fd8882621035cd
David França <davidemanuel.cf@gmail.com>
2025-11-01 20:44:22 -0300

    feat: add snackbar globally and vuequery dev tools

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
 <template>
   <v-app>
     <router-view />
+    <v-snackbar-queue
+      location="right top"
+      closable
+      close-text="X"
+      timeout="2000"
+      v-model="messages.queue"
+    ></v-snackbar-queue>
+    <VueQueryDevtools />
   </v-app>
 </template>
+
+<script setup lang="ts">
+import { VueQueryDevtools } from '@tanstack/vue-query-devtools'
+import { useMessages } from './stores/messages.store'
+
+const messages = useMessages()
+</script>
