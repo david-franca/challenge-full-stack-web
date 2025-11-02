0697ed51eddb49fe431fbf5bbdb147f4cb58de41
David França <davidemanuel.cf@gmail.com>
2025-11-02 16:09:23 -0300

    feat: add i18n to students list, header and form

--- src/locales/pt.ts
+++ src/locales/pt.ts
@@ -10,4 +10,42 @@ export const pt = {
     students: 'Alunos',
     users: 'Usuários',
   },
+  studentList: {
+    loading: 'Carregando dados... Por favor, aguarde.',
+    noData: 'Nenhum aluno encontrado.',
+    editStudent: 'Editar Aluno',
+    deleteStudent: 'Excluir Aluno',
+    headers: {
+      ra: 'RA',
+      name: 'Nome',
+      email: 'Email',
+      cpf: 'CPF',
+      actions: 'Ações',
+    },
+  },
+  studentHeader: {
+    title: 'Consulta de Alunos',
+    searchPlaceholder: 'Digite sua busca...',
+    registerStudent: 'Cadastrar Aluno',
+  },
+  studentFormDialog: {
+    createTitle: 'Cadastrar Aluno',
+    editTitle: 'Editar Aluno',
+    saveButton: 'Salvar',
+    updateButton: 'Atualizar',
+    cancelButton: 'Cancelar',
+    requiredFields: '*indica campos obrigatórios',
+    labels: {
+      name: 'Nome Completo*',
+      email: 'Email*',
+      ra: 'RA (Registro Acadêmico)*',
+      cpf: 'CPF (Cadastro de Pessoas Físicas)*',
+    },
+    rules: {
+      required: 'Este campo é obrigatório.',
+      email: 'O e-mail deve ser válido.',
+      cpfLength: 'O CPF deve conter 11 dígitos.',
+      cpfValid: 'CPF inválido.',
+    },
+  },
 }
